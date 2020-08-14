c58e7945a2c5520b1d193f78337eb827e3b7ee76
mkristie <mkristie@student.21-school.ru>
2020-08-14 19:05:24 +0300

    rdy

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,6 +1,8 @@
 #include "get_next_line.h"
 
 
+
+
 int     ft_strlen(char *str)
 {
 	int	i;
@@ -13,23 +15,17 @@ int     ft_strlen(char *str)
 	return (i);
 }
 
-static char	*remainder;
-char buf[2];
-
-
 void	allocate_first_time(char **line)
 {
-	remainder = malloc(1);
-	remainder[0] = '\0';
 	*line = malloc(1);
 	*line[0] = '\0';
 }
 
 int		ft_free_exit(int code, char **line)
 {
-	if (remainder)
+	if (*line)
 	{
-		free(remainder);
+		free(*line);
 	}
 	if (*line)
 	{
@@ -47,40 +43,47 @@ void	append_buf(char *buf, char **line)
 	i = 0;
 	j = 0;
 
-	tmp = malloc(ft_strlen(remainder) + 2);
-	while (remainder[i])
+	tmp = malloc(ft_strlen(*line) + 2);
+	while ((*line)[i])
 	{
-		tmp[i] = remainder[i];
+		tmp[i] = (*line)[i];
 		i++;
 	}
 	tmp[i] = buf[0];
 	tmp[i + 1] = '\0';
-	free(remainder);
-	remainder = tmp;
+	free(*line);
+	*line = tmp;
 }
 
 int	get_next_line(char **line)
 {
 	char buf[2];
 	int		count;
+	static int flag;
 
+    flag = 0;
 	count = 0;
 	buf[1] = '\0';
 	allocate_first_time(line);
-
 	while ((count = read(0, buf, 1)) > 0)
 	{
+	    flag = 1;
 		if (buf[0] == '\n')
+		{
 			return (1);
+		}
 		append_buf(buf, line);
 	}
 	if (count < 0)
 		ft_free_exit(-1, line);
 	else if (count == 0)
 	{
-		if (buf[0] != '\n')
+		if (flag && buf[0] != '\n')
 			append_buf(buf, line);
 		else
+		{
 			return (0);
+		}
 	}
+	return (1);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -3,9 +3,12 @@
 # define GET_NEXT_LINE_H
 
 #include <unistd.h>
+#include <stdlib.h>
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 1
 #endif
+
+int	get_next_line(char **line);
 
 #endif
