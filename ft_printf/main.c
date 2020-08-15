78e940ec60167044573dc48017d843dae8c31c2c
mkristie <mkristie@student.21-school.ru>
2020-08-15 21:23:09 +0300

    add tests for %s

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -35,17 +35,17 @@ int     main()
     // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
     // printf("\n-----------------------INTEGERS-----------------------\n\n");
-    // my = ft_printf("%d\n", 12345678);
-    // st = printf("%d\n", 12345678);
+    // my = ft_printf("|%d\n", 12345678);
+    // st = printf("|%d\n", 12345678);
     // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
-    // my = ft_printf("%d\n", 0);
-    // st = printf("%d\n", 0);
+    // my = ft_printf("|%d\n", 0);
+    // st = printf("|%d\n", 0);
     // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
-    // my = ft_printf("%d\n", 2147483647);
-    // st = printf("%d\n", 2147483647);
+    // my = ft_printf("|%d\n", 2147483647);
+    // st = printf("|%d\n", 2147483647);
     // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
-    // my = ft_printf("%d\n", -2147483647 - 1);
-    // st = printf("%d\n", -2147483647 - 1);
+    // my = ft_printf("|%d\n", -2147483647 - 1);
+    // st = printf("|%d\n", -2147483647 - 1);
     // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
     // my = ft_printf("|%.d|\n", -2147483647 - 1);
@@ -197,91 +197,132 @@ int     main()
 	// printf("32) standart PRINTF : |%.d|\n", 100);
 	// ft_printf("32) my       PRINTF : |%.d|\n", 100);
 
-    ft_printf("-----------------------HEX-----------------------\n\n\n");
-    my = ft_printf("|%x|\n", 15);
-    st = printf("|%x|\n", 15);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // ft_printf("-----------------------HEX-----------------------\n\n\n");
+    // my = ft_printf("|%x|\n", 15);
+    // st = printf("|%x|\n", 15);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%x|\n", 0);
-    st = printf("|%x|\n", 0);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%x|\n", 0);
+    // st = printf("|%x|\n", 0);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%x|\n", 123456670);
-    st = printf("|%x|\n", 123456670);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%x|\n", 123456670);
+    // st = printf("|%x|\n", 123456670);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%x|\n", 2147483647);
-    st = printf("|%x|\n", 2147483647);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%x|\n", 2147483647);
+    // st = printf("|%x|\n", 2147483647);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%.x|\n", 2147483647);
-    st = printf("|%.x|\n", 2147483647);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%.x|\n", 2147483647);
+    // st = printf("|%.x|\n", 2147483647);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%.0x|\n", 2147483647);
-    st = printf("|%.0x|\n", 2147483647);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%.0x|\n", 2147483647);
+    // st = printf("|%.0x|\n", 2147483647);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%.x|\n", 0);
-    st = printf("|%.x|\n", 0);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%.x|\n", 0);
+    // st = printf("|%.x|\n", 0);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%.0x|\n", 0);
-    st = printf("|%.0x|\n", 0);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%.0x|\n", 0);
+    // st = printf("|%.0x|\n", 0);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%.1x|\n", 0);
-    st = printf("|%.1x|\n", 0);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%.1x|\n", 0);
+    // st = printf("|%.1x|\n", 0);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%.5x|\n", 2147483647);
-    st = printf("|%.5x|\n", 2147483647);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%.5x|\n", 2147483647);
+    // st = printf("|%.5x|\n", 2147483647);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%.5x|\n", 150);
-    st = printf("|%.5x|\n", 150);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%.5x|\n", 150);
+    // st = printf("|%.5x|\n", 150);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%5.5x|\n", 150);
-    st = printf("|%5.5x|\n", 150);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+    // my = ft_printf("|%5.5x|\n", 150);
+    // st = printf("|%5.5x|\n", 150);
+    // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
-    my = ft_printf("|%1.5x|\n", 150);
-    st = printf("|%1.5x|\n", 150);
-    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
-
-
-    printf("01) standart PRINTF : |%x|\n", 34);
-	ft_printf("01) my       PRINTF : |%x|\n", 34);
-	printf("02) standart PRINTF : |%x|\n", -34);
-	ft_printf("02) my       PRINTF : |%x|\n", -34);
-	printf("04) standart PRINTF : |%10x|\n", 90);
-	ft_printf("04) my       PRINTF : |%10x|\n", 90);
-	printf("05) standart PRINTF : |%.10x|\n", 90);
-	ft_printf("05) my       PRINTF : |%.10x|\n", 90);
-	printf("06) standart PRINTF : |%15x|\n", -100);
-	ft_printf("06) my       PRINTF : |%15x|\n", -100);
-	printf("07) standart PRINTF : |%.15x|\n", -100);
-	ft_printf("07) my       PRINTF : |%.15x|\n", -100);
-	printf("08) standart PRINTF : |%10.5x|\n", 52);
-	ft_printf("08) my       PRINTF : |%10.5x|\n", 52);
-	printf("09) standart PRINTF : |%5.10x|\n", 52);
-	ft_printf("09) my       PRINTF : |%5.10x|\n", 52);
-	printf("10) standart PRINTF : |%20.15x|\n", -100000);
-	ft_printf("10) my       PRINTF : |%20.15x|\n", -100000);
-	printf("11) standart PRINTF : |%15.20x|\n", -100000);
-	ft_printf("11) my       PRINTF : |%15.20x|\n", -100000);
-	printf("17) standart PRINTF : |%.x|\n", 0);
-	ft_printf("17) my       PRINTF : |%.x|\n", 0);
-	printf("18) standart PRINTF : |%.x|\n", 88);
-	ft_printf("18) my       PRINTF : |%.x|\n", 88);
-
-
-    // printf("-----------------------STRINGS-----------------------\n\n\n");
-    // my = ft_printf("%10.2s\n", "toto");
-    // st = printf("%10.2s\n", "toto");
+    // my = ft_printf("|%1.5x|\n", 150);
+    // st = printf("|%1.5x|\n", 150);
     // if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
 
 
+    // printf("01) standart PRINTF : |%x|\n", 34);
+	// ft_printf("01) my       PRINTF : |%x|\n", 34);
+	// printf("02) standart PRINTF : |%x|\n", -34);
+	// ft_printf("02) my       PRINTF : |%x|\n", -34);
+	// printf("04) standart PRINTF : |%10x|\n", 90);
+	// ft_printf("04) my       PRINTF : |%10x|\n", 90);
+	// printf("05) standart PRINTF : |%.10x|\n", 90);
+	// ft_printf("05) my       PRINTF : |%.10x|\n", 90);
+	// printf("06) standart PRINTF : |%15x|\n", -100);
+	// ft_printf("06) my       PRINTF : |%15x|\n", -100);
+	// printf("07) standart PRINTF : |%.15x|\n", -100);
+	// ft_printf("07) my       PRINTF : |%.15x|\n", -100);
+	// printf("08) standart PRINTF : |%10.5x|\n", 52);
+	// ft_printf("08) my       PRINTF : |%10.5x|\n", 52);
+	// printf("09) standart PRINTF : |%5.10x|\n", 52);
+	// ft_printf("09) my       PRINTF : |%5.10x|\n", 52);
+	// printf("10) standart PRINTF : |%20.15x|\n", -100000);
+	// ft_printf("10) my       PRINTF : |%20.15x|\n", -100000);
+	// printf("11) standart PRINTF : |%15.20x|\n", -100000);
+	// ft_printf("11) my       PRINTF : |%15.20x|\n", -100000);
+	// printf("17) standart PRINTF : |%.x|\n", 0);
+	// ft_printf("17) my       PRINTF : |%.x|\n", 0);
+	// printf("18) standart PRINTF : |%.x|\n", 88);
+	// ft_printf("18) my       PRINTF : |%.x|\n", 88);
+
+
+    printf("-----------------------STRINGS-----------------------\n\n\n");
+    my = ft_printf("|%10.2s|\n", "toto");
+    st = printf("|%10.2s|\n", "toto");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%10.20s|\n", "toto");
+    st = printf("|%10.20s|\n", "toto");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%10.s|\n", "toto");
+    st = printf("|%10.s|\n", "toto");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%.2s|\n", "toto");
+    st = printf("|%.2s|\n", "toto");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%.s|\n", "toto");
+    st = printf("|%.s|\n", "toto");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%.0s|\n", "toto");
+    st = printf("|%.0s|\n", "toto");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%s|\n", "");
+    st = printf("|%s|\n", "");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%10s|\n", "");
+    st = printf("|%10s|\n", "");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%10.s|\n", "");
+    st = printf("|%10.s|\n", "");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%10.0s|\n", "");
+    st = printf("|%10.0s|\n", "");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    my = ft_printf("|%10.1s|\n", "");
+    st = printf("|%10.1s|\n", "");
+    if (len) {printf("ft_printf: %d, printf: %d\n", my, st);}
+
+    printf("03) standart PRINTF : |%.0%|\n");
+	ft_printf("03) my       PRINTF : |%.0%|\n");
 
 }
