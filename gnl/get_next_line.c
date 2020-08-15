481c5b44e25f19d7888bef40727cc0c519309403
mkristie <mkristie@student.21-school.ru>
2020-08-15 11:21:34 +0300

    version 3
    refactor

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,89 +1,55 @@
 #include "get_next_line.h"
 
-
-
-
 int     ft_strlen(char *str)
 {
-	int	i;
+    int i;
 
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
+    i = 0;
+    while (str[i])
+    {
+        i++;
+    }
+    return (i);
 }
 
-void	allocate_first_time(char **line)
+void    append_buf(char **line, char *buf)
 {
-	*line = malloc(1);
-	*line[0] = '\0';
+    char *tmp;
+    int i = 0;
+
+    tmp = NULL;
+    tmp = malloc(ft_strlen(*line) + 2);
+    while ((*line)[i])
+    {
+        tmp[i] = (*line)[i];
+        i++;
+    }
+    tmp[i] = buf[0];
+    tmp[i + 1] = '\0';
+    free(*line);
+    *line = tmp;
 }
 
-int		ft_free_exit(int code, char **line)
+int get_next_line(char **line)
 {
-	if (*line)
-	{
-		free(*line);
-	}
-	if (*line)
-	{
-		free(*line);
-	}
-	return (code);
-}
-
-void	append_buf(char *buf, char **line)
-{
-	char *tmp;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-
-	tmp = malloc(ft_strlen(*line) + 2);
-	while ((*line)[i])
-	{
-		tmp[i] = (*line)[i];
-		i++;
-	}
-	tmp[i] = buf[0];
-	tmp[i + 1] = '\0';
-	free(*line);
-	*line = tmp;
-}
-
-int	get_next_line(char **line)
-{
-	char buf[2];
-	int		count;
-	static int flag;
-
-    flag = 0;
-	count = 0;
-	buf[1] = '\0';
-	allocate_first_time(line);
-	while ((count = read(0, buf, 1)) > 0)
-	{
-	    flag = 1;
-		if (buf[0] == '\n')
-		{
-			return (1);
-		}
-		append_buf(buf, line);
-	}
-	if (count < 0)
-		ft_free_exit(-1, line);
-	else if (count == 0)
-	{
-		if (flag && buf[0] != '\n')
-			append_buf(buf, line);
-		else
-		{
-			return (0);
-		}
-	}
-	return (1);
+    char buf[2];
+    int     bytes;
+    
+    buf[1] = '\0';
+    *line = malloc(1);
+    (*line)[0] = '\0';
+    bytes = 0;
+    while ((bytes = read(0, buf, 1) > 0))
+    {
+        if (buf[0] == '\n')
+            return (1);
+        append_buf(line, buf);
+    }
+    if (bytes < 0)
+    {
+        if (*line)
+            free(*line);
+        return (-1);
+    }
+    return (0);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -1,14 +1,13 @@
-
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-#include <unistd.h>
-#include <stdlib.h>
+#define GET_NEXT_LINE_H
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 1
 #endif
 
-int	get_next_line(char **line);
+#include <stdlib.h>
+#include <unistd.h>
+
+int get_next_line(char **line);
 
 #endif
