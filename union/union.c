4098054b0160842ab6295296392d9f50626d0ca2
mkristie <mkristie@student.21-school.ru>
2020-08-15 11:40:17 +0300

    add union and inter

---
+++ union/union.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+int     main(int ac, char **av)
+{
+    int i = 0;
+    static unsigned char tab[127];
+
+    if (ac == 3)
+    {
+        while (av[1][i])
+        {
+            if (av[1][i] && !tab[(unsigned char)av[1][i]])
+            {
+                tab[(unsigned char)av[1][i]] = 1;
+                write(1, &av[1][i], 1);
+            }
+            i++;
+        }
+        i = 0;
+        while (av[2][i])
+        {
+            if (av[2][i] && !tab[(unsigned char)av[2][i]])
+            {
+                tab[(unsigned char)av[2][i]] = 1;
+                write(1, &av[2][i], 1);
+            }
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+}
