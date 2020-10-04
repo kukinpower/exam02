bb1e3d48a42ab06c4dde25a2f3d3114ef1c41e8c
Roman Kukin <mkristie@at-p3.msk.21-school.ru>
2020-10-04 17:53:47 +0300

    fix get_zone(), add draw()

--- exam_rank03/micro_paint.c
+++ exam_rank03/micro_paint.c
@@ -8,9 +8,28 @@ float x, y, w, h;
 char color, type;
 FILE    *fileptr;
 
+void    draw()
+{
+    int i = 0;
+    int j;
+    int hit;
+
+    while (i < width)
+    {
+        j = 0;
+        while (j < height)
+        {
+            hit = is_hit((float)i, (float)j);
+            if (hit == 2 || (hit == 1 && type == 'R'))
+                artboard[i + j * width] = color;
+        }
+    }
+}
+
 _Bool   get_zone()
 {
     int i = 0;
+    int res = 0;
 
     if (fscanf(fileptr, "%d %d %c\n", &width, &height, &bg) != 3)
         return (1);
@@ -20,14 +39,19 @@ _Bool   get_zone()
         return (1);
     while (i < width * height)
         artboard[i++] = bg; // fill array with bg char
-    
-    
+    while ((res == fscanf(fileptr, "%c %f %f %f %f %c\n", &type, &x, &y, w, &h, &color)) == 6)
+    {
+        if (w <= 0 || h <= 0 || (type != 'r' && type != 'R'))
+            return (1);
+        draw();
+    }
+    if (res != -1)
+        return (1);
+    return (0);
 }
 
 int     main(int ac, char **av)
 {
-    int res = 0;
-
     if (!(fileptr = fopen(av[1], "r")) || get_zone())
     {
         write(1, "Error: Operation file corrupted\n", 33);
@@ -39,13 +63,5 @@ int     main(int ac, char **av)
         write(1, "Error: argument\n", 16);
         return (1);
     }
-    while ((res == fscanf(fileptr, "%c %f %f %f %f %c\n", &type, &x, &y, w, &h, &color)) == 6)
-    {
-        if (w <= 0 || h <= 0 || (type != 'r' && type != 'R'))
-            return (1);
-        draw();
-    }
-    if (res != -1)
-        return (1);
-    return (0);
+    
 }
