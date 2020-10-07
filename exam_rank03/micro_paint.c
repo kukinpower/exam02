28b0a30f9d1bca74d354c8f237f120d5db7431af
Roman <thekukinpower@gmail.com>
2020-10-07 20:27:14 +0300

    re micro_paint

--- exam_rank03/micro_paint.c
+++ exam_rank03/micro_paint.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int width, height;
-char bg;
-char *artboard;
 float x, y, w, h;
-char color, type;
-FILE    *fileptr;
+char type, color, bg;
+char *artboard;
 
-int     is_hit(float i, float j)
-{
+int     check_hit(float i, float j) {
     if (i < x || i > x + w || j < y || j > y + h)
         return (0);
-    else if (i - x < 1 || x + w - i < 1 || j - y < 1 || y + h - j < 1)
+    if (i - x < 1 || x + w - i < 1 || j - y < 1 || y + h - j < 1)
         return (2);
-    else
-        return (1);
+    return (1);
 }
 
-void    draw()
+void     draw()
 {
-    int i = 0;
-    int j;
-    int hit;
+    int i = 0, j, hit;
 
-    while (i < width)
-    {
+    while (i < width) {
         j = 0;
-        while (j < height)
-        {
-            hit = is_hit((float)i, (float)j);
+        while (j < height) {
+            hit = check_hit((float)i, (float)j);
             if (hit == 2 || (hit == 1 && type == 'R'))
                 artboard[i + j * width] = color;
             j++;
@@ -38,36 +31,32 @@ void    draw()
     }
 }
 
-_Bool   get_zone()
+_Bool    parse(FILE *file)
 {
-    int i = 0;
-    int res = 0;
+    int i = 0, scan_ret;
 
-    if (fscanf(fileptr, "%d %d %c\n", &width, &height, &bg) != 3)
+    if (fscanf(file, "%d %d %c\n", &width, &height, &bg) != 3)
         return (1);
-    if (!(width > 0 && width <= 300) || !(height > 0 && height <= 300))
+    if (!(width > 0 && width <= 300 && height > 0 && height <= 300))
         return (1);
     if (!(artboard = malloc(width * height)))
         return (1);
     while (i < width * height)
-        artboard[i++] = bg; // fill array with bg char
-    while ((res == fscanf(fileptr, "%c %f %f %f %f %c\n", &type, &x, &y, w, &h, &color)) == 6)
-    {
-        if (w <= 0 || h <= 0 || (type != 'r' && type != 'R'))
+        artboard[i++] = bg;
+    while ((scan_ret = fscanf(file, "%c %f %f %f %f %c\n", &type, &x, &y, &w, &h, &color)) == 6) {
+        if (!(w > 0 && h > 0) || !(type == 'R' || type == 'r'))
             return (1);
         draw();
     }
-    if (res != -1)
+    if (scan_ret != -1)
         return (1);
     return (0);
 }
 
-void    print_artboard()
+void    output()
 {
     int i = 0;
-
-    while (i < height)
-    {
+    while (i < height) {
         write(1, artboard + i * width, width);
         write(1, "\n", 1);
         i++;
@@ -76,16 +65,18 @@ void    print_artboard()
 
 int     main(int ac, char **av)
 {
-    if (!(fileptr = fopen(av[1], "r")) || get_zone())
-    {
-        write(1, "Error: Operation file corrupted\n", 33);
-        return (1);
-    }
+    FILE *file;
 
-    if (ac != 2)
-    {
+    if (ac != 2) {
         write(1, "Error: argument\n", 16);
         return (1);
     }
-    
+    if (!(file = fopen(av[1], "r")) || parse(file)) {
+        write(1, "Error: Operation file corrupted\n", 32);
+        return (1);
+    }
+    output();
+    fclose(file);
+    free(artboard);
+    return (0);
 }
